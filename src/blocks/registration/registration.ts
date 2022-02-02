6685d65d4dc49a85026f0e8d3a41dfca5ffdb503
Artem <artema.luch@gmail.com>
2022-02-02 08:41:36 +0300

    Добавляет валидацию к регистрации

--- src/blocks/registration/registration.ts
+++ src/blocks/registration/registration.ts
@@ -4,6 +4,11 @@ import {compile} from 'pug';
 import authorizeTmpl from '../../templates/authorize.tmpl';
 import InputBlock from '../../components/input-block';
 import Link from '../../components/link';
+import Form from '../../components/form';
+import {INPUT_NAME} from '../../const';
+import Event from '../../utils/event';
+import Validation from '../../utils/validation';
+import Button from '../../components/button';
 
 export default class Registration extends Block {
   constructor(props: Record<string, unknown>) {
@@ -59,16 +64,10 @@ const passwordBlock = new InputBlock({
   name: 'password'
 });
 
-const repeatPasswordBlock = new InputBlock({
-  labelName: 'Пароль (ещё раз)',
-  inputType: 'password',
-  name: 'repeatPassword'
-});
-
-const regButton = new Link({
+const regButton = new Button({
   classNames: ['authorize__button', 'button'],
   text: 'Регистрация',
-  href: './index.html'
+  type: 'submit'
 });
 
 const enterButton = new Link({
@@ -77,14 +76,61 @@ const enterButton = new Link({
   href: './login.html'
 });
 
+const regForm = new Form({
+  wrappers: [
+    'authorize__input-wrapper',
+    'authorize__link-wrapper'
+  ],
+  events: {
+    'submit': (e) => {
+      e.preventDefault();
+
+      const target = e.target as HTMLFormElement;
+      const formData = new FormData(target);
+      const consoleObject: Record<string, string> = {};
+
+      formData.forEach((value: string, key) => {
+        switch (key) {
+          case INPUT_NAME.LOGIN:
+            Event.onFormSubmit(value, key, Validation.checkLogin, loginBlock, consoleObject);
+            break;
+          case INPUT_NAME.PASSWORD: {
+            Event.onFormSubmit(value, key, Validation.checkPassword, passwordBlock, consoleObject);
+            break;
+          }
+          case INPUT_NAME.EMAIL: {
+            Event.onFormSubmit(value, key, Validation.checkEmail, mailBlock, consoleObject);
+            break;
+          }
+          case INPUT_NAME.FIRST_NAME: {
+            Event.onFormSubmit(value, key, Validation.checkName, firstNameBlock, consoleObject);
+            break;
+          }
+          case INPUT_NAME.LAST_NAME: {
+            Event.onFormSubmit(value, key, Validation.checkName, lastNameBlock, consoleObject);
+            break;
+          }
+          case INPUT_NAME.PHONE: {
+            Event.onFormSubmit(value, key, Validation.checkPhone, telephoneBlock, consoleObject);
+            break;
+          }
+        }
+      });
+
+      // eslint-disable-next-line no-console
+      console.log(consoleObject);
+    }
+  }
+});
+
 renderDOM('.app', registration);
+renderDOM('.authorize-block', regForm);
 renderDOM('.authorize__input-wrapper', mailBlock);
 renderDOM('.authorize__input-wrapper', loginBlock);
 renderDOM('.authorize__input-wrapper', firstNameBlock);
 renderDOM('.authorize__input-wrapper', lastNameBlock);
 renderDOM('.authorize__input-wrapper', telephoneBlock);
 renderDOM('.authorize__input-wrapper', passwordBlock);
-renderDOM('.authorize__input-wrapper', repeatPasswordBlock);
 renderDOM('.authorize__link-wrapper', regButton);
 renderDOM('.authorize__link-wrapper', enterButton);
 
--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,10 @@
 export const INPUT_NAME = {
   LOGIN: 'login',
-  PASSWORD: 'password'
+  PASSWORD: 'password',
+  EMAIL: 'email',
+  FIRST_NAME: 'first_name',
+  LAST_NAME: 'last_name',
+  PHONE: 'phone',
+  REPEAT_PASSWORD: 'repeatPassword',
+  MESSAGE: 'message'
 };
